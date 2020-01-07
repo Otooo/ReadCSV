72e43b56271a3d66a4222f126007b31d0d069333
Oto Lopes <otoengc@gmail.com>
2020-01-07 18:51:24 -0300

    Init project

---
+++ src/ReadCSV.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace ReadCSV;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class ReadCSV 
+{
+    /** 
+     * Database connection
+     */ 
+    protected $connection;
+
+    /**
+     * 
+     */
+    public function __construct($conn)
+    {
+        $this->connection = $conn;
+    }
+
+    /**
+     * Reads the content of CSV file and storages
+     * in an array so then returns.
+     * 
+     * The array format is: array[row][header] => collumn
+     * 
+     * @param filepath Path of file to read
+     * @return content array of values
+     */
+    public function readFile($filepath, $delimiter=',') {
+        $content = [];
+        $header = [];
+        $headerCount = 0;
+        $row = 0;
+        if (($handle = fopen($filepath, "r")) !== false) {
+            while (($data = fgetcsv($handle, 0, $delimiter)) !== false) {
+                $num = count($data);
+                if ($row === 0) {
+                    $headerCount = $num;
+                    for ($c=0; $c < $headerCount; $c++) {
+                        $header[$c] = $data[$c] ?? "campo_desconhecido_{$c}";
+                    }
+                } else {
+                    for ($c=0; $c < $headerCount; $c++) {
+                        array_push($line, [$header[$c] => $data[$c] ?? '']);
+                    }
+                    array_push($content, $line);
+                }
+                $row++;
+            }
+        }
+        fclose($handle);
+
+        return $content;
+    }
+}
